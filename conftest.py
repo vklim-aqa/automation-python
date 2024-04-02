280d8e4abd7c214cff02cb7a8b1bb1c2ab41b010
vklim-aqa <volo.aqa@gmail.com>
2024-04-02 17:02:07 +0200

    added README

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function")
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    options = Options()
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(options=options)
+    request.cls.driver = driver
     yield driver
     driver.quit()
